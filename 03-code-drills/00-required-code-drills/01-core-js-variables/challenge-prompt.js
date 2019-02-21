ff48d0f3db95e2b3636d408497dc93ce29d6a7be
Liam Condon <condonliamt@gmail.com>
2019-02-20 23:29:19 -0800

    finish code drill 1

--- 03-code-drills/00-required-code-drills/01-core-js-variables/challenge-prompt.js
+++ 03-code-drills/00-required-code-drills/01-core-js-variables/challenge-prompt.js
@@ -20,73 +20,96 @@ meaning that any code commented out is ignored.
 // Create a variable named `subject` without giving it a value:
 // example: var hobby;
 
+var hobby;
 
 // Assign a value of "Programmers" to the variable `subject`
 // example: hobby = "problem solving"
 
+hobby = "programmers";
 
 // Create a variable `firstName` and assign it a value of an empty string.
 // example: var emptyString = "";
 
+var firstname = "";
 
 // Assign a value of "Ada" to the variable `firstName`
+
+var firstname = "Ada";
 
 
 // Create a variable `lastName` and assign it a value of a string "Lovelace".
 
+var lastName = "Lovelace";
 
 // Create a variable `fullName` and assign it a value of `firstName` and `lastName` concatenated together with a space between them.
 
+var fullName = firstname + " " + lastName;
 
 // Create a variable `profession` and assign it a value of a string, "Computer Programmer".
 
+var profession = "Computer Programmer";
 
 // Create a variable `knownFor` and assign it a value of a string, "first computer programmer".
 
+var knownFor = "first computer programmer";
 
 // Create a variable `firstAlgorithm` and assign it a value of a string, "Analytical Engine".
 
+firstAlgorithm = "Analytical Engine";
 
 // Create a variable `cityLocation` and assign it a value of a string, "London";
 
+var cityLocation = "London";
 
 // Create a variable `countryLocation` and assign it a value of a string, "England";
 
+var countryLocation = "England";
 
 // Create a variable `nationality` and assign it a value of a string, "British";
 
+var nationality = "British";
 
 
 //Topic: integers
 
 // Create a variable `birthYear` and assign it a value of the integer, 1815.
 
+var birthYear = 1815;
 
 // Create a variable `deathYear` and assign it a value of the integer, 1852.
 
+var deathYear = 1852;
 
 // Create a variable `ageAtPassing` and assign it to be the difference between `deathYear` and `birthYear`.
 
+var ageAtPassing = deathYear - birthYear;
 
 // Create a variable `yearOfPublish` and assign it a value of the integer, 1842.
 
+var yearOfPublish = 1842;
 
 // Create a variable `ageAtYearOfPublish` and assign it to be the difference between `yearOfPublish` and `birthYear`.
 
-
+var ageAtYearOfPublish = yearOfPublish - birthYear;
 
 //Topic: console log
 
 //console log: "First Name: Ada" by concatenating the `firstName` variable to the back of a string of "First Name: "
 
+console.log("First Name: " + firstname);
 
 //console log: "Last Name: Lovelace" by concatenating the `lastName` variable to the back of a string of "Last Name: "
+
+console.log("Last Name: " + lastName);
 
 
 //console log: "Profession: Computer Programmer" by concatenating the `profession` variable to the back of a string of "Profession: "
 
+console.log("Profession: " + profession);
 
 //console log: "BirthYear: 1815" by concatenating the `birthYear` variable to the back of a string of "BirthYear: "
+
+console.log("Birth Year: " + birthYear);
 
 
 
@@ -98,6 +121,7 @@ meaning that any code commented out is ignored.
 // Use the `subject`, `fullName`, `nationality`, `profession`, and `birthYear` variables instead of hard coding in the full string
 // much like you did for the console logs and `fullName`
 
+var statementOne = fullName + " is a " + nationality + " " + profession + " born in" + " " + birthYear;
 
 // Create a variable `statementTwo` and assign it a value of a string:
 // "She is commonly referred to as the first computer programmer"
@@ -105,6 +129,7 @@ meaning that any code commented out is ignored.
 // Use the `knownFor` variable instead of hard coding in the full string
 // much like you did for the console logs and `fullName`
 
+var statementTwo = "She is commonly referred to as the " + knownFor;
 
 // Create a variable `statementThree` and assign it a value of a string:
 // "In 1842 she published the first Algorithm, the Analytical Engine, at the age of 27."
@@ -112,6 +137,7 @@ meaning that any code commented out is ignored.
 // Use the `yearOfPublish`, `firstAlgorithm`, and `ageAtYearOfPublish` variables instead of hard coding in the full string
 // much like you did for the console logs and `fullName`
 
+var statementThree = "In " + yearOfPublish + " the first algorithm, " + firstAlgorithm + " athe the age of " + ageAtYearOfPublish;
 
 // Create a variable `statementFour` and assign it a value of a string:
 // "She was a British Citizen who lived in London, England until her passing in 1852 at the age of 37.
@@ -119,18 +145,28 @@ meaning that any code commented out is ignored.
 // Use the `nationality`, `cityLocation`, `countryLocation`, and `ageAtPassing` variables instead of hard coding in the full string
 // much like you did for the console logs and `fullName`
 
+var statementFour = "She was a " + nationality + " who lived in " + cityLocation + "," + countryLocation + " until her passing at " + deathYear;
 
 
 //Topic: console.log()
 
 //console log statementOne
 
+console.log(statementOne);
 
 //console log statementTwo
+
+console.log(statementTwo);
 
 
 //console log statementThree
 
 
+console.log(statementThree);
+
 //console log statementFour
+
+
+console.log(statementFour);
+
 
