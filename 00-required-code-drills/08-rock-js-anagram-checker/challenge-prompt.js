36502c23b0dabdcf26b1743b9a3ccbd9b56e364f
Liam Condon <condonliamt@gmail.com>
2019-03-08 01:09:02 -0800

    added ajax and firebase

--- 00-required-code-drills/08-rock-js-anagram-checker/challenge-prompt.js
+++ 00-required-code-drills/08-rock-js-anagram-checker/challenge-prompt.js
@@ -1,45 +1,59 @@
-// Anagrams
+// // Anagrams
 
 
-// This function takes in two strings and checks whether they're anagrams of each other.
-// Return true if they are anagrams and false if they aren't anagrams.
-function anagramCheck (str1, str2) {
+// // This function takes in two strings and checks whether they're anagrams of each other.
+// // Return true if they are anagrams and false if they aren't anagrams.
+// function anagramCheck (str1, str2) {
 
-  // ---------- Your Code Here ----------
+//   // ---------- Your Code Here ----------
  
-  str1 = str1.replace(/\s/g, "")
-  str1 = str1.toLowerCase();
-  str2 = str2.replace(/\s/g, "")
-  str2 = str2.toLowerCase();
+//   str1 = str1.replace(/\s/g, "")
+//   str1 = str1.toLowerCase();
+//   str2 = str2.replace(/\s/g, "")
+//   str2 = str2.toLowerCase();
 
-  str1 = str1.split("").sort().join("");
-  str2 = str2.split("").sort().join("");
-  // return (str1 == str2);
+//   str1 = str1.split("").sort().join("");
+//   str2 = str2.split("").sort().join("");
+//   // return (str1 == str2);
 
-  if (str1.length !== str2.length) {
-    return false;
-  }
+//   if (str1.length !== str2.length) {
+//     return false;
+//   }
 
-  for (var i=0; i < str1.length; i++) {
-    str2 = str2.replace(str1[i], "")
-  
-
-
+//   for (var i=0; i < str1.length; i++) {
+//     str2 = str2.replace(str1[i], "")
+//   }
 
 
 
-  // ----------- End Code Area -----------
 
+
+//   // ----------- End Code Area -----------
+
+// }
+
+// // This should console log true
+// console.log(anagramCheck("I am Lord Voldemort", "Tom Marvolo Riddle"));
+
+// // This should console log true
+// console.log(anagramCheck("Astronomer", "Moon Starer"));
+
+// // This should console log false
+// console.log(anagramCheck("Hello World", "Goodnight Sun"));
+
+// // This should console log false
+// console.log(anagramCheck("Supercalifragilisticexpialidocious", "If you say it loud enough"));
+
+let uglyArray = [1, [2, 3, [4, 5, [6]]]];
+let newArray = [];
+
+function prettyArray(items) {
+  items.forEach(function(items) {
+    if (Array.isArray(items)) {
+      newArray.push(...items);
+    }
+  });
+  console.log(newArray);
 }
 
-// This should console log true
-console.log(anagramCheck("I am Lord Voldemort", "Tom Marvolo Riddle"));
-
-// This should console log true
-console.log(anagramCheck("Astronomer", "Moon Starer"));
-
-// This should console log false
-console.log(anagramCheck("Hello World", "Goodnight Sun"));
-
-// This should console log false
-console.log(anagramCheck("Supercalifragilisticexpialidocious", "If you say it loud enough"));
+prettyArray(uglyArray);
