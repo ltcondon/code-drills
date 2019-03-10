8cea24101bb3e126f7680ab03b97a74cf9f37266
Liam Condon <condonliamt@gmail.com>
2019-03-10 15:13:29 -0700

    6.4

--- 04-code-drills/00-required-code-drills/03-proj-jquery-listeners-and-dynamic-generation/challenge-prompt.js
+++ 04-code-drills/00-required-code-drills/03-proj-jquery-listeners-and-dynamic-generation/challenge-prompt.js
@@ -4,33 +4,37 @@ $(document).ready(function () {
   
     // 1.1 create a variable called letters with an array of every letter in alphabet (A-Z) as a string
     
-
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     // 1.2 create a variable called body and have it equals the jquery selector of body
 
-  
+  var body = $("body")
     // 1.3 create a variable called btns and assign the result of calling the "find" method on the body variable to find the element with an id of "buttons"
 
+      var btns = body.find("#btns");
   
     // Section 2 : DYNAMICALLY the text on the button
     // =================================================================================
   
     // 2.1 Create a for-loop that iterate through the variable letters.
-
+    for (vari=0; i<letters.length; i++); {
   
       // 2.2 Create a variable named "textButton" equal to a jquery selctor of a string <button>
-
+      var textButton = $("<button>");
   
       // 2.3. use the variable textButton add following class as a string letter-button 
-
+      textButton.addClass("letter-button");
   
       // 2.4. Then give the variable text button a data-attribute of "data-letter" and the each index of letters
+      textButton.attr("data-letter", letters[i]);
 
   
       // 2.5. now give "textButton" a text of each index of letters.
+      textButton.text(letters[i]);
 
   
       // 2.6. Final step is to append "textButton" to the variable btns.
-
+      btns.append(textButton);
+    }
   
  
   
@@ -38,19 +42,22 @@ $(document).ready(function () {
     // =================================================================================
   
     // 3.1 Create an "on-click" event attached to the ".letter-button" class.
+    $(".letter-button").click(function() {
 
   
       // 3.2 Create a variable called textDiv and and set it equal to jquery selector string of <div>.
-
+      var textDiv = $("<div>");
   
       // 3.3 use the variable textDiv and add a class of a string letter.
-
+      textDiv.addClass("letter");
   
       // 3.4 use the var textDiv and add text that is equal to the this or button that creates the letter and add a attribute of a string data-letter 
-
+      textDiv.text($(this).val());
 
       // 3.5 select the body of the page and find the id of display and append textDiv to it
+      body.append(textDiv);
   
+    })
 
   });
   
