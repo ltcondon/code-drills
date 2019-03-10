8eeeb8d2cdc73d21346046185972e12af070410e
Liam Condon <condonliamt@gmail.com>
2019-03-09 16:57:55 -0800

    finished 07-firebase

--- 07-required-code-drills/06-core-firebase-chat/hint/challenge-hint.js
+++ 07-required-code-drills/06-core-firebase-chat/hint/challenge-hint.js
@@ -13,38 +13,49 @@ $(function() {
   // The first argument to the callback function we hand it will be the child
   // entry that was just added.
 
+  database.ref().on("child_added", function(childSnapshot) {
+
+
   
 
     // create a new element `chatItem` that we will append into tho `chat-display`
     // after we give it some content
-
+    var charItem = $("<p>")
     
 
     // grab the value of the child entry that was just updated. This is the
     // chat message that was stored into our database, so we can store that as
     // the contents of `chatItem`
 
+    charItem.text($(this).val)
     
 
     // Append `chatItem` to the `chat-display`
 
+
+    $("#chat-display").append(charItem);
+
+
+
+  })
     
 
 
   // Set up an on click listener on the submit button
-
-  
+$("#chat-submit").click(function() {
 
     // Grab the chat message the user has input and then clear the input area
+
+    var chatMessage = $(this).val();
+    $("#chat-input").val("");
 
     
 
     // Push the chat message into the database
-
-    
-    
-
-  // --------------------- End Code Area --------------------
+database.ref().push(chatMessage);
+  })
 
 })
+// --------------------- End Code Area --------------------
+
 
