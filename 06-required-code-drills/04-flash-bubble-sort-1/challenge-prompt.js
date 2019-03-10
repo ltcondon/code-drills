162332dd2987d9182f59a832e30c82d9571a4443
Liam Condon <condonliamt@gmail.com>
2019-03-09 19:00:49 -0800

    6.4

--- 06-required-code-drills/04-flash-bubble-sort-1/challenge-prompt.js
+++ 06-required-code-drills/04-flash-bubble-sort-1/challenge-prompt.js
@@ -4,13 +4,26 @@
 function bubbleSort(arr) {
 // ================= code goes here ===========================
 
-
-
-
-
-
-
-
+// for (var i=0; i<arr.length -1; i++) {
+//     if (arr[i] < arr[i+1]) {
+//         var tempVal = arr.splice(i+1, 1);
+//         console.log(tempVal)
+//         arr.splice(0, 0, tempVal[0]);
+//         i=0;
+//     }
+    
+// }
+//     return arr;
+    var i = 0;
+    while (i < arr.length - 1) {
+        if (arr[i] < arr[i+1]) {
+            var temp = arr.splice(i+1, 1);
+            arr.splice(i, 0, temp[0]);
+            i = 0;
+        }
+        i++;
+    }
+    return arr;
 
 // ============================================================
 }
