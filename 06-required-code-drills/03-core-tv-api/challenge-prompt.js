be3a660f4aea2a47f9cb2de641edc7b483c9c5b7
Liam Condon <condonliamt@gmail.com>
2019-03-09 18:22:12 -0800

    6.3

--- 06-required-code-drills/03-core-tv-api/challenge-prompt.js
+++ 06-required-code-drills/03-core-tv-api/challenge-prompt.js
@@ -3,11 +3,11 @@ $(document).submit(function () {
 
     // grab the show information from the form on the page
     var searchInformation;
-
+    searchInformation = $(this).val();
 
     // console.log the input
 
-
+    console.log(searchInformation);
     // this is the api we are using add your search variable to end of the queryURL
     var queryURL = "http://api.tvmaze.com/search/shows?q=";
 
@@ -16,21 +16,36 @@ $(document).submit(function () {
         method: "GET"
     }).then(function (response) {
         // console.log the response 
+        console.log(response);
 
         // create a forloop and append the first 3 responses to the page
+            for (var i=0;i<4;i++);{
+
+                console.log(response[0].show.image.medium)
+                $("#shows").append(response[0].show.image.medium)
+            
 
             // console.log the image and then append it to the page.            
-
+            console.log(response[i].show.name)
+            $("#shows").append(response[i].show.name)
 
             // console.log the name of the show and append it to the page
+            
+            console.log(response[i].show.name)
+            $("#shows").append(response[i].show.name)
 
 
             // console.log the genres of the show and append it to the page
+            
+            console.log(response[i].show.genre)
+            $("#shows").append(response[i].show.genre)
 
 
             // console.log the summary of the show and append it to the page
+            console.log(response[i].show.summary)
+            $("#shows").append(response[i].show.summary)
 
-
+            }
     });
 
 })
